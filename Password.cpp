832239cfaab3325c3c27210bb5b15ac23b8bff41
Elias Thomas Brady <etbrady42@students.tntech.edu>
2017-02-02 13:12:38 -0600

    test and added the len check portion of the function

--- Password.cpp
+++ Password.cpp
@@ -37,6 +37,11 @@ void Password::addWord(String* word)
 {
 	viable_words->add(word);
 	all_words->add(word);
+	
+	if (len == 0)
+	{
+		len = word->length();
+	}
 }
 
 void Password::guess(int try_password, int num_matches)
